3e126bbeb3229dc9ce3c0ef0afb1a7bbcb12cd53
Rodrigo Augosto <github@beecoss.com>
2023-07-19 06:12:54 -0300

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  data() {
+  },
+  mounted() {
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logoutUser')
+      .then(() => {
+        this.$router.push({ name: 'Home' })
+      })
+    }
+  }
+ }
+</script>
+
